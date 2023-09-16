9f637f1bf17fd4469040f73f62c7e74a6f587a7b
Rohit Bharadwaj <rohit@rohit.bharadwaj>
2023-09-16 21:26:59 +0530

    Update auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -3,12 +3,26 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import prisma from "./db";
 
+type CustomGoogleProviderConfig = {
+    clientId: string;
+    clientSecret: string;
+    callbacks: {
+      session: (session: any, user: any) => Promise<any>;
+    };
+  };
+
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            callbacks: {
+                session: async (session : any, user: any) => {
+                  session.id = user.id;
+                  return Promise.resolve(session);
+                },
+              },
+        } as CustomGoogleProviderConfig)
     ]
 }
